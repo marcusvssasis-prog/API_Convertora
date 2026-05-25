48f95fe14d159956fb36dc2503c06aa6903169d9
rPlakama <rPlakama@proton.me>
2026-05-25 19:35:35 -0300

    Removed: Imports desnecessarios

--- src/modules/currency/services/currency.service.ts
+++ src/modules/currency/services/currency.service.ts
@@ -3,8 +3,6 @@ import { CurrencyDto } from '../dtos/currency.dto';
 import { CreateConversionDto } from '../dtos/create-conversion.dto';
 import { ConversionRepository } from '../repositories/conversion.repository';
 import { ConversionEntity } from '../entities/conversion.entity';
-import { ICurrencyService } from '../interfaces/currency-service.interface';
-import { CurrencyRepository } from '../repositories/currency.repository';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { firstValueFrom } from 'rxjs';
@@ -17,7 +15,7 @@ export class CurrencyService {
     private readonly conversionRepository: ConversionRepository,
     private readonly configService: ConfigService,
     private readonly httpService: HttpService, // Injetando o serviço HTTP
-  ) {}
+  ) { }
 
   async calculateConversion(dto: CreateConversionDto): Promise<ConversionEntity> {
     const { amount, fromCurrency } = dto;
@@ -27,7 +25,7 @@ export class CurrencyService {
     try {
       // Recupera a chave do .env de forma segura
       const apiKey = this.configService.get<string>('HG_API_KEY');
-      // Ajuste da URL abaixo de acordo com a documentação exata da API que está sendo usada   
+      // Ajuste da URL abaixo de acordo com a documentação exata da API que está sendo usada
       const url = `https://api.hgbrasil.com/finance?key=${apiKey}`;
 
       const response = await firstValueFrom(this.httpService.get(url));
@@ -37,20 +35,20 @@ export class CurrencyService {
 
     } catch (error) {
       throw new BadRequestException(
-          `Erro ao buscar a taxa para '${fromCurrency}'. Verifique a chave da API ou a conexão.`
-        );
-      } 
+        `Erro ao buscar a taxa para '${fromCurrency}'. Verifique a chave da API ou a conexão.`
+      );
+    }
 
-const convertedResult = amount * rate;
+    const convertedResult = amount * rate;
 
-const newConversion = new ConversionEntity();
-newConversion.id = Math.random().toString(36).substring(7); 
-newConversion.amount = amount;
-newConversion.fromCurrency = fromCurrency.toUpperCase();
-newConversion.toCurrency = toCurrency;
-newConversion.rateUsed = rate;
-newConversion.result = convertedResult;
-newConversion.createdAt = new Date();
+    const newConversion = new ConversionEntity();
+    newConversion.id = Math.random().toString(36).substring(7);
+    newConversion.amount = amount;
+    newConversion.fromCurrency = fromCurrency.toUpperCase();
+    newConversion.toCurrency = toCurrency;
+    newConversion.rateUsed = rate;
+    newConversion.result = convertedResult;
+    newConversion.createdAt = new Date();
 
     return this.conversionRepository.save(newConversion);
   }
@@ -59,24 +57,24 @@ newConversion.createdAt = new Date();
     try {
       const apiKey = this.configService.get<string>('HG_API_KEY');
       const url = `https://api.hgbrasil.com/finance?key=${apiKey}`;
-      
+
       const response = await firstValueFrom(this.httpService.get(url));
       const currencies = response.data.results.currencies;
-      
+
       return Object.keys(currencies).map((key): CurrencyDto => {
         const currency = currencies[key];
-        
+
         return {
 
-        
-        bid: currency.buy ? currency.buy.toString() : '0',
-        ask: currency.sell ? currency.sell.toString() : '0',
-        timestamp: Math.floor(Date.now() / 1000),
 
-        code: key,
-        name: currency.name,
-      } as CurrencyDto; // <-- Força o compilador a aceitar que o objeto segue o formato do DTO
-    });
+          bid: currency.buy ? currency.buy.toString() : '0',
+          ask: currency.sell ? currency.sell.toString() : '0',
+          timestamp: Math.floor(Date.now() / 1000),
+
+          code: key,
+          name: currency.name,
+        } as CurrencyDto; // <-- Força o compilador a aceitar que o objeto segue o formato do DTO
+      });
     } catch (error) {
       throw new BadRequestException('Nao foi possivel buscar a lista de moedas.');
     }
